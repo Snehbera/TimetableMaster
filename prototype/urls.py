45daffcb32e0228c5918575413cdfa736fdb7cf1
unknown <aryannkacha@gmail.com>
2025-09-29 19:42:34 +0530

    ~

---
+++ prototype/urls.py
@@ -0,0 +1,34 @@
+"""
+URL configuration for prototype project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# prototype/prototype/urls.py (The main project's urls.py)
+
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect # <-- Import 'redirect'
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+# router.register(r'tasks', TaskViewSet)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('timetable/', include('timetable_app.urls')),
+    
+    # NEW LINE: Redirects the root path ('') to the desired app path.
+    path('', lambda request: redirect('timetable/generate/', permanent=False)),
+]
